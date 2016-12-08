37964fe6ed21bf4f923d3f6b10c10e55645f8bbd
nishitani yuki <yuki.nishitani85@gmail.com>
2016-12-08 20:51:50 +0900

    useless update

--- 05/src/App.js
+++ 05/src/App.js
@@ -3,14 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      isLoading: true,
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+      });
+    }, 1000);
+  }
+
   render() {
+    const { color } = this.props;
+    const { isLoading } = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {
+            isLoading
+              ? <h2>Loading</h2>
+              : <h2>Welcome to React</h2>
+          }
         </div>
-        <p className="App-intro">
+        <p style={{ color }} className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- 05/src/index.js
+++ 05/src/index.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+class MainSection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggle: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.stopPropagation();
+    const { toggle } = this.state;
+    this.setState({
+      toggle: !toggle,
+    });
+  }
+
+  render() {
+    const { color } = this.props;
+    const { toggle } = this.state;
+    const style = toggle
+      ? { color: color }
+      : { color: "black" };
+    return (
+      <div>
+        <h2 style={style}>くそお世話になりました！！！！</h2>
+        <button onClick={this.handleClick}>click here</button>
+      </div>
+    );
+  }
+}
+
+// http://example.com/colors
+const response = [
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'purple',
+];
+
+const Root = (props) => {
+  const { colors } = props;
+  return (
+    <div>
+      {
+        colors.map(color => <MainSection color={color} />)
+      }
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <App />,
+  <Root colors={response} />,
   document.getElementById('root')
 );
